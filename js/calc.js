7f0e495f6e0c9352b06fca519f037cd82242ce5e
timothy <tim.longoria@gmail.com>
2016-01-26 11:26:41 -0600

    Finished antennas for head

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-  var canvas = document.getElementById("calc-head");
-  var ctx = canvas.getContext('2d');
+  canvas = document.getElementById("calc-head");
+  ctx = canvas.getContext('2d');
   ctx.lineJoin = 'round';
   ctx.lineCap = 'round';
   ctx.lineWidth = 10.0;
   ctx.strokeStyle = '#000000';
+  drawHeadCap();
+  drawAntennas();
+  drawAntennaTips();
+  
+});
+
+function drawHeadCap()
+{
+  ctx.beginPath();
   ctx.moveTo(5, 100);
   ctx.lineTo(100, 5);
   ctx.lineTo(300, 5);
@@ -12,9 +21,50 @@ $(document).ready(function() {
   ctx.stroke();
   ctx.fillStyle = '#f5a819';
   ctx.fill();
+}
+
+function drawAntennas()
+{
+  // draw left antenna
   ctx.beginPath();
   ctx.moveTo(25, 75);
-  ctx.lineTo(10, 10);
+  ctx.lineTo(10, 20);
   ctx.lineWidth = 5.0;
+  ctx.lineTo(17, 17);
+  ctx.lineTo(45, 55);
+  ctx.fillStyle = '#bf7f00';
+  ctx.fill();
   ctx.stroke();
-});
+
+  // draw right antenna
+  ctx.beginPath();
+  ctx.moveTo(canvas.width - 25, 75);
+  ctx.lineTo(canvas.width - 10, 20);
+  ctx.lineWidth = 5.0;
+  ctx.lineTo(canvas.width - 17, 17);
+  ctx.lineTo(canvas.width - 45, 55);
+  ctx.fillStyle = '#bf7f00';
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawAntennaTips()
+{
+  // draw left antenna tip
+  ctx.beginPath();
+  ctx.arc(9.5, 9.5, 8, 0, 2*Math.PI, false);
+  ctx.fillStyle = '#f5a819';
+  ctx.fill();
+  ctx.lineWidth = 3.0;
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+
+  // draw right antenna tip
+  ctx.beginPath();
+  ctx.arc(canvas.width - 9.5, 9.5, 8, 0, 2*Math.PI, false);
+  ctx.fillStyle = '#f5a819';
+  ctx.fill();
+  ctx.lineWidth = 3.0;
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+}
